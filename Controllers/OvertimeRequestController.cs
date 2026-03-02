9c6a18f394a7b147c08b7d7d82354e1d79d623c6
HungDSHE172854 <HungDSHE172854@fpt.edu.vn>
2026-03-02 16:20:21 +0700

    Implement Create Overtime Request API

---
+++ Controllers/OvertimeRequestController.cs
@@ -0,0 +1,36 @@
+﻿using HRManagement.DTOs;
+using HRManagement.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace HRManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class OvertimeRequestController : Controller
+    {
+        private readonly IOvertimeRequestService _service;
+
+        public OvertimeRequestController(IOvertimeRequestService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateOvertimeRequestDTO dto)
+        {
+            var employeeId = int.Parse(
+                User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+
+            var result = await _service.CreateAsync(dto,employeeId);
+
+            if (!result.Success)
+                return BadRequest(result);
+
+            return Ok(result);
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@ builder.Services.AddCors(options =>
 builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
 builder.Services.AddScoped<ILeaveRequestService, LeaveRequestService>();
+builder.Services.AddScoped<IOvertimeRequestService, OvertimeRequestService>();
 builder.Services.AddAutoMapper(typeof(TaskProfile).Assembly);
 builder.Services.AddSwaggerGen(c =>
 {
