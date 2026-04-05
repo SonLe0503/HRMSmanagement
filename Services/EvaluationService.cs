63ba3fbf4296f13e046db6548d52c57fc95a914c
HoangViet <vietnhhe181621@fpt.edu.vn>
2026-04-05 11:16:40 +0700

    [update] import interface package

--- Services/EvaluationService.cs
+++ Services/EvaluationService.cs
@@ -1,6 +1,7 @@
 ﻿using HRManagement.DataAcess;
 using HRManagement.DTOs;
 using HRManagement.Models;
+using HRManagement.DataAcess.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
