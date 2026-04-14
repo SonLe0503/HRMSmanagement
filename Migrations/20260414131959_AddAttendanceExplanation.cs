82c47d85133edea879278e48c2c832083307eff0
SonLe0503 <SonLVHE172736@fpt.edu.vn>
2026-04-15 00:28:23 +0700

    [Update] fix bug

---
+++ Migrations/20260414131959_AddAttendanceExplanation.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAttendanceExplanation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExplanationMessage",
+                table: "AttendanceRecords",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExplanationResponse",
+                table: "AttendanceRecords",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExplanationStatus",
+                table: "AttendanceRecords",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExplanationMessage",
+                table: "AttendanceRecords");
+
+            migrationBuilder.DropColumn(
+                name: "ExplanationResponse",
+                table: "AttendanceRecords");
+
+            migrationBuilder.DropColumn(
+                name: "ExplanationStatus",
+                table: "AttendanceRecords");
+        }
+    }
+}
